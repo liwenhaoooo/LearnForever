7be3fb07487f404047d562800bc40d416e7dd668
wenhaoli <m18720955369@163.com>
2022-05-12 22:39:56 +0100

    1.mybatis使用示例:http://127.0.0.1:10001/system/test 2.增加三层结构 controller,mapper,service

---
+++ system/src/main/java/com/online_course/system/mapper/TestMapper.java
@@ -0,0 +1,14 @@
+package com.online_course.system.mapper;
+
+import com.online_course.system.domain.Test;
+
+import java.util.List;
+
+/**
+ * @author wenhaoli
+ * @date 2022/05/12 20:57
+ */
+public interface TestMapper {
+
+    public List<Test> list();
+}
