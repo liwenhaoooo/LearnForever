d801347560987eb638d0da0757c85da7890fdb34
wenhaoli <m18720955369@163.com>
2022-05-12 11:55:12 +0100

    Initial commit

---
+++ src/test/java/com/online_course/online_course/OnlineCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online_course.online_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
