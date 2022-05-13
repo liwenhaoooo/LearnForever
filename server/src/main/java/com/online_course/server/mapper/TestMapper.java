7dd3e3f99515174e8eca5cff76a8e726bcb9c7b9
wenhaoli <m18720955369@163.com>
2022-05-13 13:24:26 +0100

    1.新建了一个公共模块server

---
+++ server/src/main/java/com/online_course/server/mapper/TestMapper.java
@@ -0,0 +1,14 @@
+package com.online_course.server.mapper;
+
+import com.online_course.server.domain.Test;
+
+import java.util.List;
+
+/**
+ * @author wenhaoli
+ * @date 2022/05/12 20:57
+ */
+public interface TestMapper {
+
+    public List<Test> list();
+}
