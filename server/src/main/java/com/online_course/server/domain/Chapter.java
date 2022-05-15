20092d72b6846244ed216a02c510107944a7b1de
wenhaoli <m18720955369@163.com>
2022-05-15 16:44:28 +0100

    1.增加chapter列表查询接口

---
+++ server/src/main/java/com/online_course/server/domain/Chapter.java
@@ -0,0 +1,46 @@
+package com.online_course.server.domain;
+
+public class Chapter {
+    private String id;
+
+    private String course;
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", course=").append(course);
+        sb.append(", name=").append(name);
+        sb.append("]");
+        return sb.toString();
+    }
+}
