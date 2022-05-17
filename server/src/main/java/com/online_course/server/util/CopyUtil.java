54e460580dba076095b8b7021a434e5ff0936692
wenhaoli <m18720955369@163.com>
2022-05-17 15:49:26 +0100

    1.增加复制工具类copyUtil，封装BeanUtils.copyProperties,简化实体复制和列表复制的代码

---
+++ server/src/main/java/com/online_course/server/util/CopyUtil.java
@@ -0,0 +1,40 @@
+package com.online_course.server.util;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wenhaoli
+ * @date 2022/05/17 15:47
+ */
+public class CopyUtil {
+    public static <T> List<T> copyList(List source, Class<T> clazz) {
+        List<T> target = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(source)){
+            if (!CollectionUtils.isEmpty(source)){
+                for (Object c: source) {
+                    T obj = copy(c, clazz);
+                    target.add(obj);
+                }
+            }
+        }
+        return target;
+    }
+
+    public static <T> T copy(Object source, Class<T> clazz) {
+        if (source == null) {
+            return null;
+        }
+        T obj = null;
+        try {
+            obj = clazz.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        BeanUtils.copyProperties(source, obj);
+        return obj;
+    }
+}
