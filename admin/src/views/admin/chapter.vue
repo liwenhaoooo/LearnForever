9d148fd7376f30338609da5b19bcbd61f64bc7e0
wenhaoli <m18720955369@163.com>
2022-05-17 20:42:33 +0100

    1.修改Toaster组件的显示效果，更大气 2.制作了confirm组件

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -123,15 +123,15 @@ export default {
 
   },
   methods: {
-    add(){
+    add() {
       let _this = this;
-      _this.chapter ={};
+      _this.chapter = {};
       $("#form-modal").modal("show");
     },
 
-    edit(chapter){
+    edit(chapter) {
       let _this = this;
-      _this.chapter = $.extend({},chapter)
+      _this.chapter = $.extend({}, chapter)
       $("#form-modal").modal("show");
     },
     list(page) {
@@ -140,7 +140,7 @@ export default {
       _this.$ajax.post('http://127.0.0.1:10000/business/admin/chapter/list', {
         page: page,
         size: _this.$refs.pagination.size,
-      }).then((response)=>{
+      }).then((response) => {
         Loading.hide();
         console.log("查询大章列表结果：", response);
         let resp = response.data;
@@ -152,43 +152,57 @@ export default {
     save(page) {
       let _this = this;
       Loading.show();
-      _this.$ajax.post('http://127.0.0.1:10000/business/admin/chapter/save', _this.chapter).then((response)=>{
+      _this.$ajax.post('http://127.0.0.1:10000/business/admin/chapter/save', _this.chapter).then((response) => {
         Loading.hide();
         console.log("保存大章列表结果：", response);
         let resp = response.data;
-        if(resp.success){
+        if (resp.success) {
           $("#form-modal").modal("hide");
           _this.list(1);
-          toast.success("保存成功");
+          Toast.success("保存成功");
         }
       })
     },
-
     del(id) {
       let _this = this;
-      Loading.show();
-      Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          _this.$ajax.delete('http://127.0.0.1:10000/business/admin/chapter/delete/'+ id,).then((response)=>{
-            Loading.hide();
-            console.log("删除大章列表结果：", response);
-            let resp = response.data;
-            if(resp.success){
-              _this.list(1);
-             toast.success("删除成功")
-        }
-      })
-        }
-      })
+      Confirm.show("删除大章后不可恢复，确认删除？", function () {
+        Loading.show();
+        _this.$ajax.delete('http://127.0.0.1:10000/business/admin/chapter/delete/' + id).then((response) => {
+          Loading.hide();
+          console.log("删除大章列表结果：", response);
+          let resp = response.data;
+          if (resp.success) {
+            _this.list(1);
+            Toast.success("删除成功！");
+          }
+        })
+      });
     }
-  }
+    // Swal.fire({
+    //   title: '确认删除？',
+    //   text: "删除后不可恢复，确认删除？",
+    //   icon: 'warning',
+    //   showCancelButton: true,
+    //   confirmButtonColor: '#3085d6',
+    //   cancelButtonColor: '#d33',
+    //   confirmButtonText: '确认!'
+    // }).then((result) => {
+    //   if (result.value) {
+    //     Loading.show();
+    //     _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((response)=>{
+    //       Loading.hide();
+    //       console.log("删除大章列表结果：", response);
+    //       let resp = response.data;
+    //       if (resp.success) {
+    //         _this.list(1);
+    //         Toast.success("删除成功！");
+    //       }
+    //     })
+    //
+    //   }
+    // })
 }
+}
+
+
 </script>
