af1765c43c3424cb8e85ac6fd2c12a3f31a40b92
wenhaoli <m18720955369@163.com>
2022-05-18 16:14:50 +0100

    1.加上了注释 2.前端增加统一日志输出

--- business/src/main/java/com/online_course/bussiness/controller/admin/ChapterController.java
+++ business/src/main/java/com/online_course/bussiness/controller/admin/ChapterController.java
@@ -1,19 +1,15 @@
 package com.online_course.bussiness.controller.admin;
 
-import com.online_course.server.domain.Chapter;
 import com.online_course.server.dto.ChapterDto;
 import com.online_course.server.dto.PageDto;
 import com.online_course.server.dto.ResponseDto;
-import com.online_course.server.exception.ValidatorException;
 import com.online_course.server.service.ChapterService;
 import com.online_course.server.util.ValidatorUtil;
-import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-
 import javax.annotation.Resource;
-import java.util.List;
+
 
 /**
  * @author wenhaoli
@@ -27,18 +23,22 @@ public class ChapterController {
 
     @Resource
     private ChapterService chapterService;
+
+    /**
+     * 列表查询
+     */
     @PostMapping("/list")
     public ResponseDto list(@RequestBody PageDto pageDto){
-        LOG.info("pageDto: {}",pageDto);
         ResponseDto responseDto = new ResponseDto();
         chapterService.list(pageDto);
         responseDto.setContent(pageDto);
         return responseDto;
     }
-
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
     @PostMapping("/save")
     public ResponseDto save(@RequestBody ChapterDto chapterDto){
-        LOG.info("chapterDto: {}",chapterDto);
 
             // 保存校验
             ValidatorUtil.require(chapterDto.getName(), "名称");
@@ -50,10 +50,11 @@ public class ChapterController {
         responseDto.setContent(chapterDto);
         return responseDto;
     }
-
+    /**
+     * 删除
+     */
     @DeleteMapping("/delete/{id}")
-    public ResponseDto save(@PathVariable String id){
-        LOG.info("id: {}",id);
+    public ResponseDto delete(@PathVariable String id){
         ResponseDto responseDto = new ResponseDto();
         chapterService.delete(id);
         return responseDto;
--- server/src/main/java/com/online_course/server/service/ChapterService.java
+++ server/src/main/java/com/online_course/server/service/ChapterService.java
@@ -10,11 +10,11 @@ import com.online_course.server.dto.PageDto;
 import com.online_course.server.mapper.ChapterMapper;
 import com.online_course.server.util.CopyUtil;
 import com.online_course.server.util.UuidUtil;
-import org.springframework.beans.BeanUtils;
+
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
+
 import java.util.List;
 
 /**
@@ -26,23 +26,21 @@ public class ChapterService {
 
     @Resource
     private ChapterMapper chapterMapper;
-
+    /**
+     * 列表查询
+     */
     public void list(PageDto pageDto) {
         PageHelper.startPage(pageDto.getPage(),pageDto.getSize());
         ChapterExample chapterExample = new ChapterExample();
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
         PageInfo<Chapter> pageInfo = new PageInfo<>(chapterList);
         pageDto.setTotal(pageInfo.getTotal());
-//        List<ChapterDto> chapterDtoList = new ArrayList<ChapterDto>();
-//        for (int i = 0, l = chapterList.size(); i < l; i++){
-//            Chapter chapter = chapterList.get(i);
-//            ChapterDto chapterDto = new ChapterDto();
-//            BeanUtils.copyProperties(chapter,chapterDto);
-//            chapterDtoList.add(chapterDto);
-//       }
         List<ChapterDto> chapterDtoList = CopyUtil.copyList(chapterList, ChapterDto.class);
         pageDto.setList(chapterDtoList);
     }
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
     public void save(ChapterDto chapterDto) {
         Chapter chapter = CopyUtil.copy(chapterDto, Chapter.class);
         if (StringUtil.isEmpty(chapterDto.getId())) {
@@ -51,14 +49,23 @@ public class ChapterService {
             this.update(chapter);
         }
     }
+    /**
+     * 新增
+     */
     private void insert(Chapter chapter) {
         chapter.setId(UuidUtil.getShortUuid());
 
         chapterMapper.insert(chapter);
     }
+    /**
+     * 更新
+     */
     private void update(Chapter chapter) {
         chapterMapper.updateByPrimaryKey(chapter);
     }
+    /**
+     * 删除
+     */
     public void delete(String id) {
         chapterMapper.deleteByPrimaryKey(id);
     }
