0f0a8960de0c6e73512c3b76c33f14d37082eb78
wenhaoli <m18720955369@163.com>
2022-05-18 15:11:13 +0100

    1.增加工具类tool.js和校验类validator.js 2.大章中的保存功能增加非空和长度校验

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -151,6 +151,12 @@ export default {
 
     save(page) {
       let _this = this;
+      // 保存校验
+      if (!Validator.require(_this.chapter.name, "名称")
+          || !Validator.require(_this.chapter.courseId, "课程ID")
+          || !Validator.length(_this.chapter.courseId, "课程ID", 1, 8)) {
+        return;
+      }
       Loading.show();
       _this.$ajax.post('http://127.0.0.1:10000/business/admin/chapter/save', _this.chapter).then((response) => {
         Loading.hide();
