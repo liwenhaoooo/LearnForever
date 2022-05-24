33ffa904762f1f26ae0d8b29738beee076968e37
wenhaoli <m18720955369@163.com>
2022-05-24 17:00:56 +0100

    1.生成器开发：解决controller模板中的中文业务名称和模板名称的问题

--- business/src/main/java/com/online_course/business/controller/admin/SectionController.java
+++ business/src/main/java/com/online_course/business/controller/admin/SectionController.java
@@ -1,9 +1,9 @@
-package com.online_course.${module}.controller.admin;
+package com.online_course.business.controller.admin;
 
-import com.online_course.server.dto.${Domain}Dto;
+import com.online_course.server.dto.SectionDto;
 import com.online_course.server.dto.PageDto;
 import com.online_course.server.dto.ResponseDto;
-import com.online_course.server.service.${Domain}Service;
+import com.online_course.server.service.SectionService;
 import com.online_course.server.util.ValidatorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 
 
-@RequestMapping("/admin/${domain}")
+@RequestMapping("/admin/section")
 @RestController
-public class ${Domain}Controller {
-    private static final Logger LOG = LoggerFactory.getLogger(${Domain}Controller.class);
-    public static final String BUSINESS_NAME = "${tableNameCn}";
+public class SectionController {
+    private static final Logger LOG = LoggerFactory.getLogger(SectionController.class);
+    public static final String BUSINESS_NAME = "小节";
 
     @Resource
-    private ${Domain}Service ${domain}Service;
+    private SectionService sectionService;
 
     /**
      * 列表查询
@@ -26,7 +26,7 @@ public class ${Domain}Controller {
     @PostMapping("/list")
     public ResponseDto list(@RequestBody PageDto pageDto){
         ResponseDto responseDto = new ResponseDto();
-        ${domain}Service.list(pageDto);
+        sectionService.list(pageDto);
         responseDto.setContent(pageDto);
         return responseDto;
     }
@@ -34,14 +34,14 @@ public class ${Domain}Controller {
      * 保存，id有值时更新，无值时新增
      */
     @PostMapping("/save")
-    public ResponseDto save(@RequestBody ${Domain}Dto ${domain}Dto){
+    public ResponseDto save(@RequestBody SectionDto sectionDto){
 
             // 保存校验
 
 
         ResponseDto responseDto = new ResponseDto();
-        ${domain}Service.save(${domain}Dto);
-        responseDto.setContent(${domain}Dto);
+        sectionService.save(sectionDto);
+        responseDto.setContent(sectionDto);
         return responseDto;
     }
     /**
@@ -50,7 +50,7 @@ public class ${Domain}Controller {
     @DeleteMapping("/delete/{id}")
     public ResponseDto delete(@PathVariable String id){
         ResponseDto responseDto = new ResponseDto();
-        ${domain}Service.delete(id);
+        sectionService.delete(id);
         return responseDto;
     }
 }
--- generator/src/main/java/com/online_course/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/online_course/generator/server/ServerGenerator.java
@@ -2,6 +2,7 @@ package com.online_course.generator.server;
 
 import com.online_course.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
+import sun.security.pkcs11.Secmod;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -12,16 +13,21 @@ import java.util.HashMap;
  */
 public class ServerGenerator {
 
+    static  String MODULE = "business";
     static String toServicePath = "server\\src\\main\\java\\com\\online_course\\server\\service\\";
-    static String toControllerPath = "business\\src\\main\\java\\com\\online_course\\business\\controller\\admin\\" ;
+    static String toControllerPath = MODULE+"\\src\\main\\java\\com\\online_course\\"+MODULE+"\\controller\\admin\\" ;
 
 
     public static void main(String[] args) throws IOException, TemplateException {
         String Domain = "Section";
         String domain = "section";
+        String tableNameCn = "小节";
+        String module = MODULE;
         HashMap<String, Object> map = new HashMap<>();
         map.put("Domain",Domain);
         map.put("domain",domain);
+        map.put("tableNameCn", tableNameCn);
+        map.put("module",module);
         //生成service
         FreemarkerUtil.initConfig("service.ftl");
         FreemarkerUtil.generator(toServicePath+Domain+"Service.java",map);
