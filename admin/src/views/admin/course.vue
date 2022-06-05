5873aaeb261dd84317fd5a1237e969d4b0f49b5a
wenhaoli <m18720955369@163.com>
2022-06-05 17:36:03 +0100

    1.课程列表中显示讲师信息 2. 课程名称字体自适应

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -35,6 +35,18 @@
             <h3 class="search-title">
               <a href="#" class="blue">{{ course.name }}</a>
             </h3>
+
+
+            <div v-for="teacher in teachers.filter(t=>{return t.id===course.teacherId})" class="profile-activity clearfix">
+              <div>
+                <img v-show="!teacher.image" class="pull-left" src="/ace/assets/images/avatars/avatar5.png">
+                <img v-show="teacher.image" class="pull-left" v-bind:src="teacher.image">
+                <a class="user" href="#"> {{teacher.name}} </a>
+                <br>
+                {{teacher.position}}
+              </div>
+            </div>
+
             <p>
               <span class="blue bolder bigger-150">{{course.price}}&nbsp;<i class="fa fa-gbp"></i></span>
             </p>
@@ -42,7 +54,7 @@
             <p>
               <span class="badge badge-info">{{course.id}}</span>
               <span class="badge badge-info">排序：{{course.sort}}</span>
-              <span class="badge badge-info">时长：{{course.time | formatSecond}}</span>
+              <span class="badge badge-info">{{course.time | formatSecond}}</span>
             </p>
             <p>
               <button v-on:click="toChapter(course)" class="btn btn-white btn-xs btn-info btn-round">
@@ -553,6 +565,12 @@
 <style scoped>
 /*scoped : style下的样式只应用于当前组件，防止互相污染*/
   .caption h3 {
-    font-size: 20px;
+    font-size: 25px;
   }
+/*字体大小自适应，根据屏幕的分辨率显示不同大小的字体*/
+@media (max-width: 1199px) {
+  .caption h3 {
+    font-size: 16px;
+  }
+}
 </style>
