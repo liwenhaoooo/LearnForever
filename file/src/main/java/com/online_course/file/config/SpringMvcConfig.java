bdc8046295da1e6dad05169c3285092768dd12b4
wenhaoli <m18720955369@163.com>
2022-07-09 00:38:24 +0100

    1. 文件上传功能开发：讲师头像显示优化

--- file/src/main/java/com/online_course/file/config/SpringMvcConfig.java
+++ file/src/main/java/com/online_course/file/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.online_course.file.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,8 +13,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
+    @Value("${file.path}")
+    private String FILE_PATH;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/f/**").addResourceLocations("file:D:/file/online_course/course/");
+        registry.addResourceHandler("/f/**").addResourceLocations("file:" + FILE_PATH);
     }
   }
