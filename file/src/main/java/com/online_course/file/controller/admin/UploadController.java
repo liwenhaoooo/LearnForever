31633bbc62c8674de89bacda6c3897d10c3906d2
wenhaoli <m18720955369@163.com>
2022-07-12 17:23:08 +0100

    1. 大文件断点续传与极速秒传：使用文件生成md5签名，作为文件标识 2. ERROR上传视频时候出现BUG 报错代码http416 找到原因:是因为分片上传只上传了一片，导致文件损坏

--- file/src/main/java/com/online_course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/online_course/file/controller/admin/UploadController.java
@@ -4,7 +4,6 @@ import com.online_course.server.dto.FileDto;
 import com.online_course.server.dto.ResponseDto;
 import com.online_course.server.enums.FileUseEnum;
 import com.online_course.server.service.FileService;
-import com.online_course.server.util.UuidUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -46,12 +45,12 @@ public class UploadController {
                               Integer size,
                               Integer shardIndex,
                               Integer shardSize,
-                              Integer shardTotal) throws IOException {
+                              Integer shardTotal,
+                              String key) throws IOException {
         LOG.info("上传文件开始");
 
         // 保存文件到本地
         FileUseEnum useEnum = FileUseEnum.getByCode(use);
-        String key = UuidUtil.getShortUuid();
 
         //如果文件夹不存在则创建
         String dir = useEnum.name().toLowerCase();
