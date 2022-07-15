33ba45428eb7772389d2beb7f3c901c1e0005d5a
wenhaoli <m18720955369@163.com>
2022-07-15 22:10:46 +0100

    1. 用户管理与登录：增加用户名是否已存在校验 2. 增加自定义业务异常

--- server/src/main/java/com/online_course/server/service/UserService.java
+++ server/src/main/java/com/online_course/server/service/UserService.java
@@ -2,30 +2,32 @@ package com.online_course.server.service;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.github.pagehelper.util.StringUtil;
 import com.online_course.server.domain.User;
 import com.online_course.server.domain.UserExample;
-import com.online_course.server.dto.UserDto;
 import com.online_course.server.dto.PageDto;
+import com.online_course.server.dto.UserDto;
+import com.online_course.server.exception.BusinessException;
+import com.online_course.server.exception.BusinessExceptionCode;
 import com.online_course.server.mapper.UserMapper;
 import com.online_course.server.util.CopyUtil;
 import com.online_course.server.util.UuidUtil;
-
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
-
 import java.util.List;
 @Service
 public class UserService {
 
     @Resource
     private UserMapper userMapper;
+
     /**
      * 列表查询
      */
     public void list(PageDto pageDto) {
-        PageHelper.startPage(pageDto.getPage(),pageDto.getSize());
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
         UserExample userExample = new UserExample();
         List<User> userList = userMapper.selectByExample(userExample);
         PageInfo<User> pageInfo = new PageInfo<>(userList);
@@ -33,35 +35,58 @@ public class UserService {
         List<UserDto> userDtoList = CopyUtil.copyList(userList, UserDto.class);
         pageDto.setList(userDtoList);
     }
+
     /**
      * 保存，id有值时更新，无值时新增
      */
     public void save(UserDto userDto) {
         User user = CopyUtil.copy(userDto, User.class);
-        if (StringUtil.isEmpty(userDto.getId())) {
+        if (StringUtils.isEmpty(userDto.getId())) {
             this.insert(user);
-        }else {
+        } else {
             this.update(user);
         }
     }
+
     /**
      * 新增
      */
     private void insert(User user) {
-
         user.setId(UuidUtil.getShortUuid());
+        User userDb = this.selectByLoginName(user.getLoginName());
+        if (userDb != null) {
+            throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+        }
         userMapper.insert(user);
     }
+
     /**
      * 更新
      */
     private void update(User user) {
         userMapper.updateByPrimaryKey(user);
     }
+
     /**
      * 删除
      */
     public void delete(String id) {
         userMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 根据登录名查询用户信息
+     * @param loginName
+     * @return
+     */
+    public User selectByLoginName(String loginName) {
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andLoginNameEqualTo(loginName);
+        List<User> userList = userMapper.selectByExample(userExample);
+        if (CollectionUtils.isEmpty(userList)) {
+            return null;
+        } else {
+            return userList.get(0);
+        }
     }
 }
---
+++ system/src/main/java/com/online_course/system/controller/ControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.online_course.system.controller;
+
+import com.online_course.server.dto.ResponseDto;
+import com.online_course.server.exception.BusinessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author wenhaoli
+ * @date 2022/07/15 21:28
+ */
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
+    @ExceptionHandler(value = BusinessException.class)
+    @ResponseBody
+    public ResponseDto businessExceptionHandler(BusinessException e) {
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.setSuccess(false);
+        LOG.error("业务异常：{}", e.getCode().getDesc());
+        responseDto.setMessage(e.getCode().getDesc());
+        return responseDto;
+    }
+}
