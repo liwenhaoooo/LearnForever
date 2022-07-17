385c2e6e1f5e2b2b41c8535cd1c95c8b21cdf6fe
wenhaoli <m18720955369@163.com>
2022-07-17 15:37:18 +0100

    1. 通用权限管理：资源树的显示

--- system/src/main/java/com/online_course/system/controller/admin/ResourceController.java
+++ system/src/main/java/com/online_course/system/controller/admin/ResourceController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 
 @RequestMapping("/admin/resource")
@@ -54,6 +55,16 @@ public class ResourceController {
     public ResponseDto delete(@PathVariable String id){
         ResponseDto responseDto = new ResponseDto();
         resourceService.delete(id);
+        return responseDto;
+    }
+    /**
+     * 资源树查询
+     */
+    @GetMapping("/load-tree")
+    public ResponseDto loadTree() {
+        ResponseDto responseDto = new ResponseDto();
+        List<ResourceDto> resourceDtoList = resourceService.loadTree();
+        responseDto.setContent(resourceDtoList);
         return responseDto;
     }
 }
