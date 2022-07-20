858271d1a366599df7b440dcb0657784ea602353
wenhaoli <m18720955369@163.com>
2022-07-20 20:52:07 +0100

    1. 将数据库中的数据翻译为英文

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -484,22 +484,7 @@
           <div class="footer-content">
 						<span class="bigger-120">
 							<span class="blue bolder">Admin</span>
-							在线视频课程 &copy; 2022-2022
-						</span>
-
-            &nbsp; &nbsp;
-            <span class="action-buttons">
-							<a href="#">
-								<i class="ace-icon fa fa-twitter-square light-blue bigger-150"></i>
-							</a>
-
-							<a href="#">
-								<i class="ace-icon fa fa-facebook-square text-primary bigger-150"></i>
-							</a>
-
-							<a href="#">
-								<i class="ace-icon fa fa-rss-square orange bigger-150"></i>
-							</a>
+							Learn_Forever &copy; 2022-2022
 						</span>
           </div>
         </div>
